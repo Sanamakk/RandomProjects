e9a561ecd42e4991d3947a5e8ee473721a1778ff
Lucas H. Silva <109255012+Sanamakk@users.noreply.github.com>
2023-01-27 21:34:39 -0300

    Add files via upload

---
+++ graph-composer-master/README.md
@@ -0,0 +1,6 @@
+# Markov Chain Composer
+Using Markov Chain to represent relationships between words in song lyrics and then generating new lyrics.. *ahem* interpretive poetry... from the graph
+
+To run: `python3 compose.py`
+
+
---
+++ graph-composer-master/graph_template.py
@@ -0,0 +1,42 @@
+import random
+
+class Vertex(object):
+    def __init__(self, value):
+        pass
+
+    def add_edge_to(self, vertex, weight=0):
+        pass
+
+    def increment_edge(self, vertex):
+        pass
+
+    def get_adjacent_nodes(self):
+        pass
+
+    # initializes probability map
+    def get_probability_map(self):
+        pass
+
+    def next_word(self):
+        pass
+
+
+
+class Graph(object):
+    def __init__(self):
+        pass
+
+    def get_vertex_values(self):
+        pass
+
+    def add_vertex(self, value):
+        pass
+
+    def get_vertex(self, value):
+        pass
+
+    def get_next_word(self, current_vertex):
+        pass
+
+    def generate_probability_mappings(self):
+        pass
